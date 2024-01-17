c9edb06ca7fba0a4029cd34833c76ec5d13e654e
PETER IRUNGU <peteralidante254@gmail.com>
2024-01-17 17:41:22 +0300

    Add theme styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from "react";
-
+import Theme from "./styled/theme/Theme.styled";
+import ElevatedButton from "./styled/button/ElevatedButton.styled";
+import OutlinedButton from "./styled/button/OutlinedButton.styled";
+import PrimaryButton from "./styled/button/PrimaryButton.styled";
+import SecondaryButton from "./styled/button/SecondaryButton.styled";
 
 const App = () => {
-  
-
   return (
-    <>
-      <h1>HELLO STYLED-TSX</h1>
-    </>
+    <Theme
+      themeType="dark"
+      child={
+        <>
+          <h1>Elevated Button</h1>
+          <ElevatedButton>Elevated Button</ElevatedButton>
+          <h1>Outlined Button</h1>
+          <OutlinedButton>Outlined Button</OutlinedButton>
+          <h1>Primary Button</h1>
+          <PrimaryButton>Primary Button</PrimaryButton>
+          <h1>Secondary Button</h1>
+          <SecondaryButton> Secondary Button</SecondaryButton>
+        </>
+      }
+    />
   );
 };
 
